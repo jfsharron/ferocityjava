43203c8dcbd0a0dbcb41df674a411a7ecc7bff6d
Jonas Sharron 205 <jfsharron@msn.com>
2019-09-03 10:31:11 -0700

    first commit

---
+++ src/ferocityjava/FerocityJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ferocityjava;
+
+/**
+ *
+ * @author jfsha
+ */
+public class FerocityJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
